ce3fea4a172ca05cc03e5d892edc0a15d928c31e
Alvin Xiao <Alvin.Xiao@warriorlife.net>
2017-01-17 22:29:15 -0800

    I added a few more function button, PI, E and more!

--- calculator.js
+++ calculator.js
@@ -7,14 +7,36 @@ function d(val) {
 
 // Type numbers and operators
 function v(val) {
-	document.getElementById("d").value += val;
+	//document.getElementById("d").value += val;
+	var x = document.getElementById("d").value; 
+	var y = x + val; //dfgcx6
+	document.getElementById("d").value = y;
 }
 
 // Evaluate the equation 
 function e() {
 	try {
-		d(eval(document.getElementById("d").value));
+		//d(eval(document.getElementById("d").value));
+		var x = document.getElementById("d").value;
+		var y = eval(x);
+		d(y);
 	} catch(err) {
 		d("Error");
 	}
+}
+
+// Square Root
+function sqrt() {
+	var f  = document.getElementById("d").value;
+	var x = eval(f);
+	var y = Math.sqrt(x);
+	d(y);
+}
+
+// Squared
+function sq() {
+	var f  = document.getElementById("d").value;
+	var x = eval(f);
+	var y = x * x;
+	d(y);
 }
